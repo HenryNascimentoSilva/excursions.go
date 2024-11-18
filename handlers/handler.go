4e8243a6643bd3fca9412f97ca1f0e03d2ce91b6
henrynascimentosilva <henrynascimentosilva@gmail.com>
2024-11-18 00:17:12 -0300

    fix: separing handler content

--- handlers/handler.go
+++ handlers/handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"excursion.com/config"
+	"excursion.com/schemas"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -13,35 +14,61 @@ var (
   db *gorm.DB
 )
 
-func ShowExcursionHandler(ctx *gin.Context) {
-  ctx.JSON(http.StatusOK, gin.H {
-    "msg": "showing excursion",
-  })
-}
 
-func DeleteExcursionHandler(ctx *gin.Context) {
-  id := ctx.Param("id")
-  ctx.JSON(http.StatusOK, gin.H {
-    "msg": "delete excursion",
-    "id": id,
-  })
-}
+
 
 func UpdateExcursionHandler(ctx *gin.Context) {
-  id := ctx.Param("id")
+  req := UpdateExcursionRequest{}
+
+  ctx.BindJSON(&req)
+
+  if err := req.Validate(); err != nil {
+    logger.Errorf("validation erro: %v", err.Error())
+    sendError(ctx, http.StatusBadRequest, err.Error())
+    return
+  }
+
+  id :=   ctx.Query("id")
+  if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+  excursion := schemas.Excursion{}
+
+  if err := db.First(&excursion, id).Error; err != nil {
+    sendError(ctx, http.StatusInternalServerError, "excursion not found")
+    return
+  }
+
+  if req.Image != "" {
+    excursion.Image = req.Image
+  }
+  if req.Title != "" {
+    excursion.Title = req.Title
+  }
+  if req.Description != "" {
+    excursion.Description = req.Description
+  }
+  if req.Buy != "" {
+    excursion.Buy = req.Buy
+  }
+  if req.FindMore != "" {
+    excursion.FindMore = req.FindMore
+  }
+
+  if err := db.Save(&excursion).Error; err != nil {
+    logger.Errorf("error updating excursion %v", err.Error())
+    sendError(ctx, http.StatusInternalServerError, "error updating excursion")
+    return
+  }
+
   ctx.JSON(http.StatusOK, gin.H {
-    "msg": "updating excursion",
-    "id": id,
+    "updated data": excursion,
   })
 }
 
-func ShowExcursionsHandler(ctx *gin.Context) {
-  id := ctx.Param("id")
-  ctx.JSON(http.StatusOK, gin.H {
-    "msg": "showing all excursions",
-    "id": id,
-  })
-}
+
 
 func InitalizeHandler() {
   logger = config.GetLogger("handler")
